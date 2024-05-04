ff766f679462a4ca0ed2a42d3863179dc5f3cc30
marzuk <marzuk777@gmail.com>
2024-05-05 01:25:28 +0600

    src folder added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {
     generateCategorySql,
     generateContent,
     generateContentTags
- } from "./readCSVAndGenerateSql";
+ } from "./src/readCSVAndGenerateSql";
 
  generateTagsSql();
  generateCategorySql();
